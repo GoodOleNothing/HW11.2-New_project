0326e03c801e60265b447908812043fddaf24552
GoodOleNothing <alexgog77777@gmail.com>
2023-02-07 04:02:21 +0500

    ДЕЛАЮ МИНИМАЛЬНЫЕ УЛУЧШЕНИЯ В ХОТФИКС!!!

--- str_func.py
+++ str_func.py
@@ -1,3 +1,6 @@
+#ДЕЛАЮ МИНИМАЛЬНЫЕ УЛУЧШЕНИЯ В ХОТФИКС
+
+
 def str_f():
     user = input()
     return print(f'{user.upper()}')
@@ -9,6 +12,6 @@ str_f()
 def str_t():
     user = input()
     return print(f'{user.title()}')
-"""Делает заглавные буквы"""
+"""МЕНЯЮ НЕПРАВИЛЬНЫЙ ДОКСТРИНГ"""
 
 str_t()
